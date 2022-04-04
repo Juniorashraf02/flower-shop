6ccc20bb83ae7ec9b519a5d32786375503783bbf
juniorashraf02 <shuvo.istiak11@gmail.com>
2022-04-04 07:20:45 +0600

    blogs-section-complete

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -4,7 +4,19 @@ import { Link } from 'react-router-dom';
 const Blogs = () => {
     return (
         <Link to='/blogs'>
-            <h1>This is blogs</h1>
+            <div className="container mx-auto m-5">
+                <div className="text-justify  border rounded-lg p-3">
+                    <h1 className="text-3xl font-medium mb-5">1. What is context api?</h1>
+                    <p className="text-gray-600">The React Context API is a way for a React structure that enables one to exchange unique details and assists in solving prop-drilling from all levels of one's application. In other words, The React Context API is a way for a React app to efectively produce gobal vriables that can be passed around. And this is alternative to moving props or props-drilling from grandparent to child to parent and so on. Context API is a (kind of) new feature added in version 16.3 of React that allows one to share state across the entire app (or part of it) lightly and with ease. And this context API will not replace redux entriely. For using API context we need to use React.createContext(). It returs a consumer and a provider. Provider is  a component that as it's names suggests provides the state to its children. It will hold the store and be the parent of all the components that might need that store. Consumer as it so happens is a component that consumes and uses the state. More information can be found on React's documentation page.
+                    </p>
+                </div>
+                <div className="text-justify  border rounded-lg p-3 mt-5">
+                    <h1 className="text-3xl font-medium mb-5">2. What is semantic tag?</h1>
+                    <p className="text-gray-600">
+                        we know that HTML is a markup language and HTML was originally created as a markup language to describe documents on the early internet.Day by day the internet got many user and it grew a lot and for this it needs to change.Semantic HTML tags provide information aboutthe contents of those tags that goes beyond just how they look on a page.Text that is enclosed in the open and closing tag is immediately recognized by the browser as some type of coding language.By adding semantic tag we can add additional information to our website which aids us in communication and helps Google and Bing understand the roles and relative importance of the different parts of my page. Specially semantic tags make it clear to the browser what the meaning of a page and its content is. There are many semantic tags. Some of them are: article, asidedetails, figcaption, figure, footer, header, main, mark,nav, section, summary, time.
+                    </p>
+                </div>
+            </div>
         </Link>
     );
 };
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,53 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { BsStarFill, BsStarHalf } from 'react-icons/bs';
 
 const Home = () => {
     return (
         <Link to='/home'>
-             <h2>This is home</h2>
+            <div>
+                <div className="md:flex container mx-auto m-2 p-2 items-center justify-around">
+                    <div>
+                        <h1 className="text-4xl text-justify   font-extrabold text-orange-400 mb-2">We sell the best flowers <br /> <p className="text-sky-700"> for you in any proggrame!</p></h1>
+
+                        <p className="text-gray-400 text-justify mb-5">We have varaities collection of flowers. We have our own flower garder. We collect the best condition flowers for our customer. Our company has opened three new store in London area to serve our valuable customers. </p>
+                    </div>
+                    <div className=" md:ml-5">
+                        <img className="rounded-xl justify-center" src={require('../images/flower.png')} alt="this is a flower" />
+                    </div>
+                </div>
+                <div className="mt-5 container mx-auto">
+                    <h1 className="text-4xl font-bold text-orange-500">Customer reviews (3)</h1>
+                    <div className="flex justify-around mt-5 text-justify gap-12 ">
+                        <div className="w-1/2 border rounded-lg p-5">
+                            <p className="font-medium">Istiak</p>
+                            <p className="text-gray-600">Flowers are very lively. Thank you! Recommending this shop.</p>
+                           <p className="text-yellow-600">
+                                <small className="flex items-center"> <p className="mr-2">Ratings:</p> <BsStarFill /><BsStarFill /><BsStarFill /><BsStarFill /> <BsStarFill /> <p className="ml-2">(5/5)</p>
+                                </small>
+                            </p>
+                        </div>
+                        <div className="w-1/2 border rounded-lg p-5">
+                            <p className="font-medium">Ashraf</p>
+                            <p className="text-gray-600">Flowers has nice smelling! but the delivery was a little bit slow. Overall good!!!</p>
+                            <p className="text-yellow-600">
+                                <small className="flex items-center"> <p className="mr-2">Ratings:</p> <BsStarFill /><BsStarFill /><BsStarFill /><BsStarFill /> <BsStarHalf /> <p className="ml-2">(4.5/5)</p>
+                                </small>
+                            </p>
+                        </div>
+                        <div className="w-1/2 border rounded-lg p-5">
+                            <p className="font-medium">Shuvo</p>
+                            <p className="text-gray-600">I am recovering this shop for good quality products in cheap rate. Keep it up you guyz!!!</p>
+                            <p className="text-yellow-600">
+                                <small className="flex items-center"> <p className="mr-2">Ratings:</p> <BsStarFill /><BsStarFill /><BsStarFill /><BsStarFill /> <BsStarFill /> <p className="ml-2">(5/5)</p>
+                                </small>
+                            </p>
+                        </div>
+                    </div>
+                    <Link to="/reviews">
+                        <button className="rounded bg-blue-700 m-5 text-white px-5 py-1 mt-5">See all reviews</button></Link>
+                </div>
+            </div>
         </Link>
     );
 };
